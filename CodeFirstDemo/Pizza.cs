4ae05c8250cae08569aaccd935990f6b56f22c09
saswat887 <86560895+saswat887@users.noreply.github.com>
2024-09-21 08:58:35 +0530

    Add files via upload

---
+++ CodeFirstDemo/Pizza.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    [Table("pizzabl")]
+    public class Pizza
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int pid { get; set; }
+        [Column(TypeName ="varchar(20)")]
+        public string pizzanames { get; set; }
+        public int price { get; set; }
+        [Column("pizzatype",TypeName = "varchar(20)")]
+        public string ptype { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string description { get; set; }
+    }
+}
